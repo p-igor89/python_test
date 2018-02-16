0071f079067d825e575f47cf18cb1728f2751bfb
p-igor89 <iperetyatko@techincubator.com.au>
2018-02-16 17:16:04 +0200

    (DEV) A check of location group element in the group list

--- fixture/group.py
+++ fixture/group.py
@@ -58,3 +58,8 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         # Submit group creation
         self.return_to_group_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_group_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,2 +1,7 @@
+from model.group import Group
+
+
 def test_del_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test'))
     app.group.del_first_group()
